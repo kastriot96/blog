595232506c2944da3bb0cbc44ee44d238cc5dbb0
kastriot96 <79404857+kastriot96@users.noreply.github.com>
2021-07-31 01:32:58 +0200

    new features

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,37 @@
-from django.http.response import HttpResponse
 from django.shortcuts import render
+from datetime import date
 
+all_posts = [
+    {   
+        "slug" : "hike-in-the-mountains",
+        "image" : "mountains.jpg",
+        "author" : "Kastriot",
+        "date" : date(2021, 8 , 1), 
+        "title": "Mountain Hiking",
+        "excerpt" : "DUMMY TEXT" ,
+        "content" : "MORE DUMMY TEXT",
+    }
+]
+
+def get_date(post):
+    return post['date'] # or post.get('date')
 
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key= get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts" : latest_posts
+    })
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html", {
+        "all_posts" : all_posts
+    })
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post" : identified_post
+    })

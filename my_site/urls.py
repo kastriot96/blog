11de8123c42080d993ba560a8c8280294ac99e94
kastriot96 <79404857+kastriot96@users.noreply.github.com>
2021-08-01 16:01:27 +0200

    final

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,7 +1,6 @@
 """my_site URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,6 +16,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
-    path("blog/", include("blog.urls")), #if someone visits localhost:8000/blog/ > lead to staring page
+    path('admin/', admin.site.urls),
+    path("", include("blog.urls"))
 ]

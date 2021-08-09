d5a888959084c493968975b84a7a44720df0ae09
kastriot96 <79404857+kastriot96@users.noreply.github.com>
2021-08-09 22:22:27 +0200

    update

--- my_site/settings.py
+++ my_site/settings.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +53,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / "templates"
+            BASE_DIR / "templates",
+            BASE_DIR / "blog/template",
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -77,8 +77,7 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
-
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
